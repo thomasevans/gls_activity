c79a96b37b7fc023ed0d2280356d0d48c197483b
Tom Evans <thomas.jude.evans@gmail.com>
2016-02-23 17:35:13 +0100

    A few notes etc

--- activ_plot_instructions2.R
+++ activ_plot_instructions2.R
@@ -1,7 +1,7 @@
 # Make an activity plot from a GLS activity (*.act) file
 
 # Install required packages (if you don't have):
-install.packages(c("ggplot2", "maptools", "scales"))
+install.packages(c("ggplot2", "maptools", "scales", "lubridate"))
 
 # Set the working directory (where your files are located)
 setwd("")
@@ -9,12 +9,19 @@ setwd("")
 # Source the function 'activ.plot'
 source("activ_plot2_broken.R")
 
+
+# NB to see accepted arguments for 'time_zone' run the following:
+OlsonNames()
+
 # Run plot function
 activ.plot(act_file = "gls10049_20120625_000.act",
            trn_file = "gls10049_20120625_000_thresh_10.trn",
-           trn_true = TRUE,
+           trn_true = FALSE,
            col.wet = "black", col.dry = "white",
-           time_zone = "UTC", long = 18.0, lat = 57.25)
+           time_zone = "EST", long = 18.0, lat = 57.25)
+
+# timezone thing doesn't seem to be working properly - time wrong for sunrise/set, and
+# not wrapping properly for either.
 
 # Notes
 # act_file - specify the file name
